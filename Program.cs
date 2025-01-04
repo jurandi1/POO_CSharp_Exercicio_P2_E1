3931c2845cb568e57027a7ca3f35ed942d38fc10
Jurandi Junior <jurandijr20@gmail.com>
2025-01-04 18:40:17 -0300

    Exercício concluído

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
-﻿internal class Program
+﻿using POO_CSharp_Exercicio_P2_E1;
+using System;
+using System.Globalization;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        //EXERCÍCIO 01:
+        //Fazer um programa para ler os valores da largura e altura de um retângulo. Em
+        //seguida, mostrar na tela o valor de sua área, perímetro e diagonal.Usar uma classe
+        //como mostrado no projeto ao lado.
+
+        Retangulo ret = new Retangulo();
+
+        Console.WriteLine("Entre a largura e altura do retângulo: ");
+        ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("AREA = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("PERIMETRO = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
     }
 }
---
+++ Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace POO_CSharp_Exercicio_P2_E1
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
